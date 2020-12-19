bf123e2f0d77808ec817bebaedf4f1513cb88052
wargin-E <62258266+wargin-E@users.noreply.github.com>
2020-12-19 11:06:01 +0900

    Update balanced_world(4949).py

--- balanced_world(4949).py
+++ balanced_world(4949).py
@@ -1,103 +1,44 @@
-sen_list=[]
-simple_sen_list=[]
+sentences=[]
+simple_sentences=[]
+answers=[]
+brackets=["(",")","[","]"]
 while True:
-    a=input()
-    if a==".":
+    k=input()
+    if k==".":
         break
     else:
-        sen_list.append(a)
-def erase(a):
-    a=list(a)
-    bracket_list=["(",")","{","}","[","]"]
-    temp_list=[]
-    for k in a:
-        if k in bracket_list:
-            temp_list.append(k)
-    a=temp_list
-    return a
-def tersmall(a):
-    l=len(a)
-    before=len(a)
-    for k in range(0,l-1):
-        if a[k]=="(" and a[k+1]==")":
-            a[k+1]=0
-            a[k]=0
+        sentences.append(k)
+for k in sentences:
+    k=list(k)
+    for a in range(0,len(k)):
+        if k[a] not in brackets:
+            k[a]=0
+    while 0 in k:
+        k.remove(0)
+    simple_sentences.append(k)
+def terminate(k):
+    for s in range(0,len(k)-1):
+        if k[s]=="[" and k[s+1]=="]":
+            k[s]=0
+            k[s+1]=0
+        elif k[s]=="(" and k[s+1]==")":
+            k[s]=0
+            k[s+1]=0
+    while 0 in k:
+        k.remove(0)
+for k in simple_sentences:
+    while True:
+        if len(k)==0 or len(k)==1:
             break
-    while 0 in a:
-        a.remove(0)
-    after=len(a)
-    if before==after:
-        return 0
-    else:
-        return 1
-def termedium(a):
-    l=len(a)
-    before=len(a)
-    for k in range(0,l-1):
-        if a[k]=="{" and a[k+1]=="}":
-            a[k+1]=0
-            a[k]=0
+        l=len(k)
+        terminate(k)
+        if l==len(k):
             break
-    while 0 in a:
-        a.remove(0)
-    after=len(a)
-    if before==after:
-        return 0
-    else:
-        return 1
-def terbig(a):
-    l=len(a)
-    before=len(a)
-    for k in range(0,l-1):
-        if a[k]=="[" and a[k+1]=="]":
-            a[k+1]=0
-            a[k]=0
-            break
-    while 0 in a:
-        a.remove(0)
-    after=len(a)
-    if before==after:
-        return 0
-    else:
-        return 1
-def remove_brackets(s):
-    before=len(s)
-    while "(" in s and ")" in s:
-        if s[0] == ")":
-            break
-        else:
-            tersmall(s)
-            if tersmall(s) == 0:
-                break
-    while "{" in s and "}" in s:
-        if s[0] == "}":
-            break
-        else:
-            termedium(s)
-            if termedium(s) == 0:
-                break
-    while "[" in s and "]" in s:
-        if s[0] == "]":
-            break
-        else:
-            terbig(s)
-            if terbig(s) == 0:
-                break
-    after=len(s)
-    if before==after:
-        return 0
-for k in sen_list:
-    simple_sen_list.append(erase(k))
-for s in simple_sen_list:
-    while remove_brackets(s)!=0:
-        remove_brackets(s)
-
-for k in simple_sen_list:
+for k in simple_sentences:
     if len(k)==0:
         print("yes")
     else:
         print("no")
-
 
 
 
